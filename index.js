60fa2bab0737323724fa6aec0b387499513326f4
Md_Rasel <raselmondream245@gmail.com>
2026-09-25 22:44:43 +0600

    post api create for all products page

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors")
 const { MongoClient } = require("mongodb");
 
 dotenv.config();
 
 const app = express();
+app.use(cors())
+app.use(express.json())
 const port = process.env.PORT;
 
 const client = new MongoClient(process.env.MONGODB_URI);
@@ -17,6 +20,50 @@ async function connectToMongoDB() {
     console.log("✅ MongoDB connected successfully!");
 
     const db = client.db("amar-fosol");
+    const productCollection=db.collection("products")
+
+    // add product api
+    app.post('/product', async (req,res)=>{
+      const productData = req.body
+      const result =await productCollection.insertOne(productData)
+      res.send(result)
+    })
+
+
+    // all product get api
+
+    app.get('/product',async (req,res)=>{
+      const result = await productCollection.find().toArray()
+      res.send(result)
+    })
+
+    // one product api for products details 
+    app.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     console.log("✅ Database selected:", db.databaseName);
 
